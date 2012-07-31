903614afa5c9cb74758ebc11ab7aab7fcc769bba
DonEvans <don9450-register@yahoo.com>
2012-07-30 18:59:56 -0700

    production.rb modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ MessageBoard2::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+	# Modified to true by me to fix heroku
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
